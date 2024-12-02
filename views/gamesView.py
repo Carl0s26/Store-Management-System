320afaa72cf6931e08366d077e4511d5fc446bfe
Carl0s26 <Carloskepp26@gmail.com>
2024-12-02 13:26:33 -0400

    Created the basic flet router
    I created the first model of the system that allows us to alternate between the different views, Missing the designs of each view and the implementation of the router on the new designs

---
+++ views/gamesView.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+#! FINISH the view desing
+
+def games_View(router):
+    #* defining view content
+    
+    content = ft.Column([
+        ft.Text("cmweicnwejcnw")
+    ])
+    return content
